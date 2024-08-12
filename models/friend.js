1ce8360735a5e99cfdb661af83806814fc129cf3
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2024-08-12 18:25:23 +0300

    adjust validations

--- models/friend.js
+++ models/friend.js
@@ -4,18 +4,17 @@ const friendSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        max: 20
+        maxLength: [20, "name is too long"]
     },
     date: {
         type: Number,
         min: 200, // These numbers will be changed later
-        max: 2070,
+        max: [2024, "We didn't meet in the future"],
         required: true
     },
     note: {
         type: String,
         required: false,
-        max: 100
     }
 })
 
